2027ddc8ca7e97974c1e4994c84b8aaed17f7326
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-07-14 13:50:23 +0700

    Messages component

---
+++ src/components/Messages/Messages.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Messages.scss";
+import Message from "../Message";
+const Messages = () => {
+  return (
+    <div className="messages">
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+    </div>
+  );
+};
+
+export default Messages;
