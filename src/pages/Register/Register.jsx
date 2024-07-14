b4795f6d521961ffa39ea7f4254513c35b4de66d
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-07-14 09:04:02 +0700

    Register pages

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Register.scss";
+import { RiImageAddFill } from "react-icons/ri";
+
+const Register = () => {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Lama Chat</span>
+        <span className="title">Register</span>
+        <form action="">
+          <input type="text" placeholder="display name" />
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <RiImageAddFill id="addImg" />
+            <span>Add an avatar</span>
+          </label>
+          <button>Sign up</button>
+        </form>
+        <p>You do have an account? Login</p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
